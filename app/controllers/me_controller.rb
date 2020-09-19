a615d20248faf2fb6e9c92f845b73681d2037f39
Evgeny Burdaev <hate2me@yandex.ru>
2020-09-19 18:38:57 +0300

    name

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -2,6 +2,8 @@ class MeController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render json: current_user
+    render json: current_user.attributes.merge(
+      name: "#{current_user.first_name} #{current_user.last_name}"
+    )
   end
 end
