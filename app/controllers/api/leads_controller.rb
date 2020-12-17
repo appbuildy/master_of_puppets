3d4704c366fe10904896c02e7caeca99b910c223
Evgeny Burdaev <hate2me@yandex.ru>
2020-12-17 22:30:40 +0300

    resp

--- app/controllers/api/leads_controller.rb
+++ app/controllers/api/leads_controller.rb
@@ -4,6 +4,8 @@ module Api
   class LeadsController < BaseController
     def create
       Lead.create(user_params.slice(:role, :need, :email).to_h)
+
+      render json: { ok: true }
     end
 
     private
