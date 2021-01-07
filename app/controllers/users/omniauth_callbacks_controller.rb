eca5449f8ce5b632f698ffac75417a619ec6884f
Evgeny Burdaev <hate2me@yandex.ru>
2021-01-07 16:30:23 +0300

    google auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,6 +14,20 @@ module Users
       end
     end
 
+    def google_oauth2
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if @user.persisted?
+        sign_in(resource_name, resource)
+
+        jwt = request.env['warden-jwt_auth.token']
+        redirect_to "https://build.appbuildy.com/oauth?jwt=#{jwt}"
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url
+      end
+    end
+
     def failure
       redirect_to root_path
     end
