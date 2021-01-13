d42d800205e19ecaf54cb6b6c487ecc485898a52
Evgeny Burdaev <hate2me@yandex.ru>
2021-01-13 17:51:20 +0300

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def add_www_subdomain
     unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}x.com#{request.request_uri}",
+      redirect_to("#{request.protocol}appbuildy.com#{request.request_uri}",
                   :status => 301)
     end
   end
