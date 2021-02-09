d7ebf809afcdcd6660f306656f1d1b953730e43a
Evgeny Burdaev <hate2me@yandex.ru>
2021-02-10 00:59:37 +0300

    FIX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist, omniauth_providers: %i[facebook google_oauth2]
+  devise :omniauthable,:jwt_authenticatable,
+         :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,  jwt_revocation_strategy: JwtBlacklist, omniauth_providers: %i[facebook google_oauth2]
 
   has_many :projects
 
