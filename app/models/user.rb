b0bb5d658bb8817856a73edacf7ab977442284e5
Evgeny Burdaev <hate2me@yandex.ru>
2021-02-10 00:19:07 +0300

    only jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable,
-         :database_authenticatable, :registerable,
+  devise :omniauthable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist, omniauth_providers: %i[facebook google_oauth2]
 
