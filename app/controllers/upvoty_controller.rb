61d47bb89bd74211a16118e0317898527cfe4009
Evgeny Burdaev <hate2me@yandex.ru>
2021-02-16 20:30:13 +0300

    Commited

--- app/controllers/upvoty_controller.rb
+++ app/controllers/upvoty_controller.rb
@@ -3,5 +3,7 @@ class UpvotyController < ApplicationController
 
   def show
     response.headers.delete "X-Frame-Options"
+    payload = { name: params[:email_id], email: params[:email_id], id: params[:email_id] }
+    @jwt = JWT.encode(payload, '5413037c5a37c08a06bd5dc6e5002928', 'HS256')
   end
 end
